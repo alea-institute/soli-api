c14d3ac21a09a7e78ef273c861129ea494869c68
Michael Bommarito <michael.bommarito@gmail.com>
2024-11-09 07:40:50 -0500

    adding llm search to api

--- soli_api/api.py
+++ soli_api/api.py
@@ -1,17 +1,19 @@
 """Main API module to define the FastAPI app and its configuration"""
 
-# Standard library imports
+# imports
+import logging
+import os
 from contextlib import asynccontextmanager
 from typing import Any, Dict
 
+# packages
 import uvicorn
-
-# Third-party imports
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from soli import SOLI
+from alea_llm_client import OpenAIModel, AnthropicModel, VLLMModel
 
-# Project imports
+# project imports
 import soli_api.routes.info
 import soli_api.routes.root
 import soli_api.routes.search
@@ -31,26 +33,97 @@ async def lifespan_handler(app_instance: FastAPI):
     """
     # Initialize the SOLI graph
     app_instance.state.config = load_config()
-    app_instance.state.soli = initialize_soli(app_instance.state.config["soli"])
+
+    # get log level
+    log_level = {
+        "info": logging.INFO,
+        "debug": logging.DEBUG,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL,
+    }.get(
+        app_instance.state.config.get("log_level", "info").lower().strip(), logging.INFO
+    )
+
+    # set up the logger at api.log
+    app_instance.state.logger = logging.getLogger("soli_api")
+    app_instance.state.logger.setLevel(log_level)
+    log_handler = logging.FileHandler("api.log")
+    log_handler.setLevel(log_level)
+    log_formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    log_handler.setFormatter(log_formatter)
+    app_instance.state.logger.addHandler(log_handler)
+
+    # initialize the SOLI instance
+    app_instance.state.soli = initialize_soli(
+        app_instance.state.config["soli"],
+        app_instance.state.config["llm"],
+    )
+
+    # log it
+    app_instance.state.logger.info(
+        "SOLI instance initialized with llm %s", app_instance.state.soli.llm.model
+    )
 
     yield
 
+    # log shutdown
+    app_instance.state.logger.info("Shutting down API")
 
-def initialize_soli(soli_config: Dict[str, Any]) -> SOLI:
+
+def initialize_soli(soli_config: Dict[str, Any], llm_config: Dict[str, Any]) -> SOLI:
     """Initialize SOLI instance based on configuration
 
     Args:
         soli_config (Dict[str, Any]): SOLI configuration dictionary
+        llm_config (Dict[str, Any]): LLM configuration dictionary
 
     Returns:
         SOLI: Initialized SOLI instance
     """
+    # initialize an llm
+    llm_engine = llm_config.get("type", "openai").lower().strip()
+    llm_model = llm_config.get("model", "gpt-4o").lower().strip()
+    llm_endpoint = llm_config.get("endpoint", None)
+    llm_api_key = llm_config.get("api_key", os.getenv("OPENAI_API_KEY"))
+
+    # create the llm
+    if llm_engine in ("openai",):
+        llm_args = {
+            "model": llm_model,
+            "api_key": llm_api_key,
+        }
+        if llm_endpoint is not None:
+            llm_args["endpoint"] = llm_endpoint
+        llm = OpenAIModel(**llm_args)
+    elif llm_engine in ("anthropic",):
+        llm_args = {
+            "model": llm_model,
+            "api_key": llm_api_key,
+        }
+        if llm_endpoint is not None:
+            llm_args["endpoint"] = llm_endpoint
+        llm = AnthropicModel(**llm_args)
+    elif llm_engine in ("vllm",):
+        llm_args = {
+            "model": llm_model,
+            "api_key": llm_api_key,
+        }
+        if llm_endpoint is not None:
+            llm_args["endpoint"] = llm_endpoint
+        llm = VLLMModel(**llm_args)
+    else:
+        llm = None
+
     return SOLI(
         source_type=soli_config["source"],
         github_repo_owner=soli_config["repository"].split("/")[0],
         github_repo_name=soli_config["repository"].split("/")[1],
         github_repo_branch=soli_config["branch"],
         use_cache=True,
+        llm=llm,
     )
 
 
--- soli_api/routes/search.py
+++ soli_api/routes/search.py
@@ -14,6 +14,26 @@ from soli_api.models.owl import OWLClassList, OWLSearchResults
 # API router
 router = APIRouter(prefix="/search", tags=["search"])
 
+# set min and max query length defaults
+MIN_QUERY_LENGTH = 2
+MAX_QUERY_LENGTH = 1024
+
+# default depth
+DEFAULT_MAX_DEPTH = 3
+
+
+def query_length_check(query: str) -> bool:
+    """
+    Check if the query string is at least 2 characters long.
+
+    Args:
+        query (str): Query string
+
+    Returns:
+        bool: True if the query string is at least 2 characters long
+    """
+    return MIN_QUERY_LENGTH <= len(query) <= MAX_QUERY_LENGTH
+
 
 @router.get("/prefix", tags=["search"], response_model=OWLClassList)
 async def search_prefix(request: Request, query: str) -> OWLClassList:
@@ -27,8 +47,8 @@ async def search_prefix(request: Request, query: str) -> OWLClassList:
     Returns:
         OWLClassList: Pydantic model with list of classes
     """
-    # make sure that the query string is at least 2 characters long to avoid attacks
-    if len(query) < 2:
+    # check query length
+    if not query_length_check(query):
         return OWLClassList(classes=[])
 
     soli: SOLI = request.app.state.soli
@@ -45,8 +65,12 @@ async def search_label(request: Request, query: str) -> OWLSearchResults:
         query (str): Query string
 
     Returns:
-        OWLClassList: Pydantic model with list of classes
+        OWLSearchResults: Pydantic model with list of classes
     """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
     soli: SOLI = request.app.state.soli
     return OWLSearchResults(results=soli.search_by_label(query))
 
@@ -63,5 +87,493 @@ async def search_definition(request: Request, query: str) -> OWLSearchResults:
     Returns:
         OWLClassList: Pydantic model with list of classes
     """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
     soli: SOLI = request.app.state.soli
     return OWLSearchResults(results=soli.search_by_definition(query))
+
+
+@router.get("/llm/area-of-law", tags=["search"], response_model=OWLSearchResults)
+async def search_llm_area_of_law(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI areas of law.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth of the search
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_areas_of_law(max_depth=max_depth)
+        )
+    )
+
+
+@router.get("/llm/asset-types", tags=["search"], response_model=OWLSearchResults)
+async def search_asset_types(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI asset types.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_asset_types(max_depth=max_depth)
+        )
+    )
+
+
+# communication modalities
+@router.get(
+    "/llm/communication-modalities", tags=["search"], response_model=OWLSearchResults
+)
+async def search_communication_modalities(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI communication modalities.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query,
+            search_set=soli.get_communication_modalities(max_depth=max_depth),
+        )
+    )
+
+
+# get currencies
+@router.get("/llm/currencies", tags=["search"], response_model=OWLSearchResults)
+async def search_currencies(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI currencies.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_currencies(max_depth=max_depth)
+        )
+    )
+
+
+# data formats
+@router.get("/llm/data-formats", tags=["search"], response_model=OWLSearchResults)
+async def search_data_formats(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI data formats.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_data_formats(max_depth=max_depth)
+        )
+    )
+
+
+# document artifacts
+@router.get("/llm/document-artifacts", tags=["search"], response_model=OWLSearchResults)
+async def search_document_artifacts(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI document artifacts.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_document_artifacts(max_depth=max_depth)
+        )
+    )
+
+
+# engagement terms
+@router.get("/llm/engagement-terms", tags=["search"], response_model=OWLSearchResults)
+async def search_engagement_terms(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI engagement terms.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_engagement_terms(max_depth=max_depth)
+        )
+    )
+
+
+# events
+@router.get("/llm/events", tags=["search"], response_model=OWLSearchResults)
+async def search_events(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI events.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_events(max_depth=max_depth)
+        )
+    )
+
+
+# governmental bodies
+@router.get(
+    "/llm/governmental-bodies", tags=["search"], response_model=OWLSearchResults
+)
+async def search_governmental_bodies(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI governmental bodies.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_governmental_bodies(max_depth=max_depth)
+        )
+    )
+
+
+# industries
+@router.get("/llm/industries", tags=["search"], response_model=OWLSearchResults)
+async def search_industries(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI industries.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_industries(max_depth=max_depth)
+        )
+    )
+
+
+# legal authorities
+@router.get("/llm/legal-authorities", tags=["search"], response_model=OWLSearchResults)
+async def search_legal_authorities(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI legal authorities.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_legal_authorities(max_depth=max_depth)
+        )
+    )
+
+
+# locations
+@router.get("/llm/locations", tags=["search"], response_model=OWLSearchResults)
+async def search_locations(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI locations.
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_locations(max_depth=max_depth)
+        )
+    )
+
+
+# matter narratives
+@router.get("/llm/matter-narratives", tags=["search"], response_model=OWLSearchResults)
+async def search_matter_narratives(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI matter narratives.
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_matter_narratives(max_depth=max_depth)
+        )
+    )
+
+
+# matter narrative formats
+@router.get(
+    "/llm/matter-narrative-formats", tags=["search"], response_model=OWLSearchResults
+)
+async def search_matter_narrative_formats(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI matter narrative formats.
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query,
+            search_set=soli.get_matter_narrative_formats(max_depth=max_depth),
+        )
+    )
+
+
+# objectives
+@router.get("/llm/objectives", tags=["search"], response_model=OWLSearchResults)
+async def search_objectives(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI objectives.
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_objectives(max_depth=max_depth)
+        )
+    )
+
+
+@router.get("/llm/player-actors", tags=["search"], response_model=OWLSearchResults)
+async def search_player_actors(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI player actors.
+
+    Args:
+        request (Request): FastAPI request object
+        query (str): Query string
+        max_depth (int): Maximum depth
+
+    Returns:
+        OWLClassList: Pydantic model with list of classes
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_player_actors(max_depth=max_depth)
+        )
+    )
+
+
+# standards compatibilities
+@router.get(
+    "/llm/standards-compatibilities", tags=["search"], response_model=OWLSearchResults
+)
+async def search_standards_compatibilities(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI standards compatibilities.
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query,
+            search_set=soli.get_standards_compatibilities(max_depth=max_depth),
+        )
+    )
+
+
+# statuses
+@router.get("/llm/statuses", tags=["search"], response_model=OWLSearchResults)
+async def search_statuses(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI statuses.
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_statuses(max_depth=max_depth)
+        )
+    )
+
+
+# system identifiers
+@router.get("/llm/system-identifiers", tags=["search"], response_model=OWLSearchResults)
+async def search_system_identifiers(
+    request: Request, query: str, max_depth: int = DEFAULT_MAX_DEPTH
+) -> OWLSearchResults:
+    """
+    Get class information using the SOLI system identifiers.
+    """
+    # check query length
+    if not query_length_check(query):
+        return OWLClassList(classes=[])
+
+    soli: SOLI = request.app.state.soli
+    return OWLSearchResults(
+        results=await soli.search_by_llm(
+            query=query, search_set=soli.get_system_identifiers(max_depth=max_depth)
+        )
+    )
